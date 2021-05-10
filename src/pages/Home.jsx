4ca52fd54904c9a954f341b54f30bfe13d790b39
Stewart Wong <siwei.wong@gmail.com>
2021-05-10 22:12:32 +0800

    implement simple sort and search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
-import { Container, Grid } from "@material-ui/core"
+import { Container, Grid, MenuItem, TextField } from "@material-ui/core"
 import React, { useEffect } from "react"
 
 import { Loading } from "../components/Loading"
 import { ProductCard } from "../components/ProductCard"
 import { getAllProducts } from "../api/product"
+import { makeStyles } from "@material-ui/core"
 import { useState } from "react"
 
+const useStyles = makeStyles((theme) => ({
+  searchBox: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
 export const Home = () => {
+  const classes = useStyles()
   const [products, setProducts] = useState(null)
+  const [filtered, setFiltered] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState("")
+  const [sort, setSort] = useState("product")
 
   useEffect(() => {
     let mounted = true
@@ -16,6 +27,17 @@ export const Home = () => {
       if (mounted) {
         const dbProducts = await getAllProducts()
         setProducts(dbProducts)
+        setFiltered(() =>
+          dbProducts.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+              return -1
+            }
+            if (a.title.toLowerCase() > b.title.toLowerCase()) {
+              return 1
+            }
+            return 0
+          })
+        )
         setLoading(false)
       }
     }
@@ -25,6 +47,72 @@ export const Home = () => {
     }
   }, [])
 
+  const handleSearch = (e) => {
+    const value = e.target.value
+    if (value === "") {
+      setFiltered(products)
+    }
+    setSearch(value)
+    if (products) {
+      setFiltered(() => {
+        return products.filter((item) => {
+          switch (sort) {
+            case "product":
+              return item.title.toLowerCase().includes(value.toLowerCase())
+
+            case "price":
+              return String(item.price).includes(value.toLowerCase())
+
+            case "category":
+              return item.category.toLowerCase().includes(value.toLowerCase())
+
+            default:
+              return null
+          }
+        })
+      })
+    }
+  }
+
+  const handleSort = (e) => {
+    switch (e.target.value) {
+      case "product":
+        setFiltered((state) =>
+          state.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+              return -1
+            }
+            if (a.title.toLowerCase() > b.title.toLowerCase()) {
+              return 1
+            }
+            return 0
+          })
+        )
+        break
+      case "price":
+        setFiltered((state) => state.sort((a, b) => a.price - b.price))
+        break
+
+      case "category":
+        setFiltered((state) =>
+          state.sort((a, b) => {
+            if (a.category.toLowerCase() < b.category.toLowerCase()) {
+              return -1
+            }
+            if (a.category.toLowerCase() > b.category.toLowerCase()) {
+              return 1
+            }
+            return 0
+          })
+        )
+        break
+
+      default:
+        break
+    }
+    setSort(e.target.value)
+  }
+
   if (loading) {
     return <Loading />
   }
@@ -32,8 +120,31 @@ export const Home = () => {
   return (
     <Container>
       <Grid container spacing={1}>
-        {products &&
-          products.map((product) => (
+        <Grid item xs={10}>
+          <TextField
+            className={classes.searchBox}
+            fullWidth
+            label='Search'
+            value={search}
+            variant={"outlined"}
+            onChange={handleSearch}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            className={classes.searchBox}
+            fullWidth
+            select
+            label='sort by'
+            value={sort}
+            onChange={handleSort}>
+            <MenuItem value='product'>product</MenuItem>
+            <MenuItem value='price'>price</MenuItem>
+            <MenuItem value='category'>category</MenuItem>
+          </TextField>
+        </Grid>
+        {filtered &&
+          filtered.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
       </Grid>
